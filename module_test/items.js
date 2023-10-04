de96f1cc068fb555ecf2be5fe9c4a613024f6325
frogBear_7 <{220098tt@yse-c.net}>
2023-10-04 10:24:05 +0900

    module require

--- module_test/items.js
+++ module_test/items.js
@@ -1,7 +1,8 @@
 // 2023-10-04 3.week3
 
+// モジュール化
 exports.items = [
     'Apple',
     'Orange',
-    'Peach'
+    'Peach',
 ]
--- module_test/main.js
+++ module_test/main.js
@@ -1,1 +1,8 @@
 // 2023-10-04 3.week3
+
+// モジュールの読み込み
+const itemModule = require('./items')
+const controlModule = require('./control')
+
+var items = itemModule.items
+controlModule.show(items)
