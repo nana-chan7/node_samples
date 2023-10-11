95dfafa56879f09a8d34f2e9b59374b5153fda66
frogBear_7 <{220098tt@yse-c.net}>
2023-10-11 10:03:00 +0900

    up

--- express_sample/server.js
+++ express_sample/server.js
@@ -27,6 +27,10 @@ app.get('/', (req, res) =>{
     res.send('Hello!!!!!')
 })
 
+app.get('/profile', (req, res) =>{
+    res.send('Profile Page')
+})
+
 // ※ サーバー停止: 起動中のターミナルで Ctrl + C
 // サーバ待機 (Listen)
 app.listen(PORT, HOST, () =>{
