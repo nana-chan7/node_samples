cb30390dc0cadfb44a5a7aa1b6afb24c31d4035d
frogBear_7 <{220098tt@yse-c.net}>
2023-11-01 09:46:11 +0900

    item.js削除・Itemクラス作成

--- express_sample/models/Model.js
+++ express_sample/models/Model.js
@@ -28,3 +28,27 @@ class Model {
 
 // モジュール化
 module.exports = Model
+
+
+// models/item.js 
+// // 2023-10-18 3.week5
+// // fsモジュールを読み込み
+// const fs = require('fs')
+// // data/items.jsonのパス設定
+// exports.filePath = "./data/items.json"
+
+// // すべてのデータを取得するメソッド
+// exports.get = () => {
+//     // 外部ファイルの読み込み
+//     var json = fs.readFileSync(this.filePath)
+//     // JSONデータをパース(オブジェクトに変換)
+//     var values = JSON.parse(json)
+//     return values
+// }
+
+// // IDを指定してデータを取得するメソッド
+// exports.find = (id) => {
+//     var values = this.get()
+//     // データを繰り返して、idが一致したら返す
+//     return values.find((value) => value.id == id)
+// } 
--- express_sample/models/item.js
+++ express_sample/models/item.js
@@ -1,24 +1,9 @@
-// 2023-10-18 3.week5
+// 2023-11-01 3.week7
 
-// fsモジュールを読み込み
-const fs = require('fs')
+// Modelモジュールを読み込み
+const Model = require('./Model')
 
-// data/items.jsonのパス設定
-exports.filePath = "./data/items.json"
-
-// すべてのデータを取得するメソッド
-exports.get = () => {
-    // 外部ファイルの読み込み
-    var json = fs.readFileSync(this.filePath)
-    // JSONデータをパース(オブジェクトに変換)
-    var values = JSON.parse(json)
-
-    return values
+//Modelクラスを継承
+class Item extends Model {
+    dataFile = "./data/items.json" // ※メインプログラムからの相対パス
 }
-
-// IDを指定してデータを取得するメソッド
-exports.find = (id) => {
-    var values = this.get()
-    // データを繰り返して、idが一致したら返す
-    return values.find((value) => value.id == id)
-} 
