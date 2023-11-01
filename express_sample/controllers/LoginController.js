7b2bdf4b0ac04a43cc7b615314d0cb72510a0c1d
frogBear_7 <{220098tt@yse-c.net}>
2023-11-01 11:14:09 +0900

    修正・UserController作成

--- express_sample/controllers/LoginController.js
+++ express_sample/controllers/LoginController.js
@@ -1,5 +1,7 @@
 // 2023-10-25 3.week6
+// 2023-11-01 3.week7
 
+const User = require('../models/Users')
 // 入力画面 (TOP)
 exports.index = (req, res) => {
     // views/login/index.ejsを表示
@@ -9,21 +11,37 @@ exports.index = (req, res) => {
 // ログイン処理
 // POSTリクエスト 裏でログイン名とパスワードを送信する
 exports.auth = (req, res) =>{
-    var loginName = req.body.login_name
+    // POSTデータの受け取り
+    var email = req.body.login_name
     var password = req.body.password
-    console.log(loginName, password)
 
-    var message = "ログイン失敗"
+    // ユーザ認証
+    const user = new User()
+    const authUser = user.auth(email, password)
+
+    if (authUser) {
+        // 認証ユーザがいれば、ユーザホームにリダイレクト
+        res.redirect('/user')
+    }else {
+        // 認証が失敗したら、ログインページにリダイレクト
+        res.redirect('/login')
+    }
+
+    // var loginName = req.body.login_name
+    // var password = req.body.password
+    // console.log(loginName, password)
+
+    // var message = "ログイン失敗"
     // .env で設定した値で、ログインチェック
     // TODO: データベースに接続してユーザ取得
     // TODO: パスワードはハッシュ値でチェック
-    if (loginName == process.env.LOGIN_NAME
-        && password == process.env.PASSWORD){
-            message = "ログイン成功"
+    // if (loginName == process.env.LOGIN_NAME
+    //     && password == process.env.PASSWORD){
+    //         message = "ログイン成功"
         // TODO: ログインが成功したらユーザの状態を保存
         // TODO: ログイン後のページへ転送
-    } else{
-        // TODO: ログイン画面に戻す
-    }
-    res.send(message)
+    // } else{
+    //     // TODO: ログイン画面に戻す
+    // }
+    // res.send(message)
 }
--- express_sample/models/Users.js
+++ express_sample/models/Users.js
@@ -6,7 +6,24 @@ const Model = require('./Model')
 //Modelクラスを継承
 class User extends Model { // Modelクラス: スーパークラス
     dataFile = "./data/users.json" // ※メインプログラムからの相対パス
+
+    // ↓ 単体テストで使われる？
+    /**
+     * 認証用のメソッド
+     * @param {*} email 
+     * @param {*} password 
+     * @return array
+     */
+    auth = (email, password) => {
+        // email と password が一致したらユーザを返す
+        // TODO: データベースに接続してユーザ取得
+        // TODO: パスワードはハッシュ値でチェック
+        return this.get().find((value) => 
+            (value.email == email && value.password == password))
+    }
 }
 
 // モジュール化 
 module.exports = User
+
+// 単体テスト = Unit Test
