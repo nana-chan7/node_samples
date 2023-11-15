0db10e53ad590d92c960a9016975d4752ab4f86f
frogBear_7 <{220098tt@yse-c.net}>
2023-11-15 12:14:29 +0900

    up

--- express_sample/models/User.js
+++ express_sample/models/User.js
@@ -1,31 +1,46 @@
 // 2023-11-15 3.week8
 
-// mysql/promiseモジュールを読み込み
+// Terminal: npm i bcrypt
+
+//mysql2/promiseモジュール読み込み
 const mysql = require('mysql2/promise')
 const db = require('../lib/db')
+//bcryptモジュール
+const bcrypt = require('bcrypt')
 
-class User { 
-    // ユーザ追加 (非同期処理)
+class User {
+    //ユーザ追加（非同期処理）
     add = async (post) => {
-        // DB接続
+        //DB接続
+        //パスワードをハッシュ化
+        post.password = bcrypt.hashSync(post.password, 10);
+
         var result;
         try {
             const con = await mysql.createConnection(db.info);
-            // SQL実行
+            //SQL実行
             var sql = `INSERT INTO users SET ?;`
             result = con.query(sql, post);
-            con.end();  
+            con.end();
         } catch (error) {
-            
-        } finally {
-            
+
         }
         return result;
     }
-    auth = (email, password) => {
+    auth = async (email, password) => {
+        var result;
+        try {
+            const con = await mysql.createConnection(db.info);
+            //SQL実行 (email 検索)
+            var sql = `SELECT * FROM users WHERE ?;`
+            const [rows, files]
+            con.end();
 
+        } catch (error) {
+            
+        }
     }
-};
+}
 
-// モジュール化 
+//モジュール化
 module.exports = User
